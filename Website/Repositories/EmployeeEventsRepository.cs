83b54b1cc6fcf61a87a0c51b89b172cc69c40b90
Josh Fryer <josh.fryer2@gmail.com>
2024-08-27 03:50:31 +0100

    add missing files

---
+++ Website/Repositories/EmployeeEventsRepository.cs
@@ -0,0 +1,77 @@
+﻿using Dapper;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel;
+using System.Data;
+using System.Threading;
+using Website.Models;
+using Website.Persistence;
+
+namespace Website.Repositories
+{
+    public class EmployeeEventsRepository : RepositoryBase
+    {
+        public EmployeeEventsRepository(IDbConnectionProvider connectionProvider) : base(connectionProvider)
+        {
+        }
+
+        /// <summary>
+        ///     Insert attending event for employee
+        /// </summary>
+        public async Task InsertAsync(int eventId, int employeeId, CancellationToken cancellationToken = default)
+        {
+            var parameters = new
+            {
+                employeeId,
+                eventId
+            };
+
+            var command = new CommandDefinition(
+                "[api].[spEmployeeEventsInsert]", // TO DO
+                parameters: parameters,
+                commandType: CommandType.StoredProcedure,
+                cancellationToken: cancellationToken);
+
+            await Connection.ExecuteAsync(command);
+        }
+
+        /// <summary>
+        ///     Delete attending event for employee
+        /// </summary>
+        public async Task DeleteAsync(int eventId, int employeeId, CancellationToken cancellationToken = default)
+        {
+            var parameters = new
+            {
+                employeeId,
+                eventId
+            };
+
+            var command = new CommandDefinition(
+                "[api].[spEmployeeEventsDelete]", // TO DO
+                parameters: parameters,
+                commandType: CommandType.StoredProcedure,
+                cancellationToken: cancellationToken);
+
+            await Connection.ExecuteAsync(command);
+        }
+
+        /// <summary>
+        ///     Determines if a Employee Event exists
+        /// </summary>
+        public async ValueTask<bool> ExistsAsync(int eventId, int employeeId, CancellationToken cancellationToken = default)
+        {
+            var parameters = new
+            {
+                employeeId,
+                eventId
+            };
+
+            var command = new CommandDefinition(
+                "[api].[spEmployeeEventsExists]",
+                parameters: parameters,
+                commandType: CommandType.StoredProcedure,
+                cancellationToken: cancellationToken);
+
+            return await Connection.ExecuteScalarAsync<bool>(command);
+        }
+    }
+}
